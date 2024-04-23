e6c30606c9f11cf5cc33958335427eb926070477
Yahya <yahyaoulha0@gmail.com>
2024-04-23 21:00:34 +0100

    worked

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const result = [];
+
+  try {
+      result.push(mathFunction());
+  } catch (err) {
+      result.push(String(err));
+  } finally {
+      result.push('Guardrail was processed');
+  }
+
+  return result;
+}
