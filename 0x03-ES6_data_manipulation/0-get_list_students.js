5f79cf36eeb87ebe936d12c8b15dd63ba39e63a0
yahia <yahyaoulha0@gmail.com>
2024-05-12 13:24:34 +0100

    worked

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
 export default function getListStudents() {
-  let StudentsObj = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" }
+  const StudentsObj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return StudentsObj;
-
 }
